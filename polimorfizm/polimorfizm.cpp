b26325441533e6cc77a3b930dce29c13f0d189b5
tikale <lekawa77@gmail.com>
2019-06-21 00:26:45 +0200

    3 rodzaje

--- polimorfizm/polimorfizm.cpp
+++ polimorfizm/polimorfizm.cpp
@@ -10,10 +10,26 @@ int main()
 	float a1 = 3.4f;
 	float b1 = 5.7f;
 
-	cout << sum(a, b);
+	cout << sum(a, b); // dla int bêdzie to funkcja int
 	cout << "\n";
-	cout << sum(a1, b1);
+	cout << sum(a1, b1); // dla float bedzie to funkcja float
 	cout << "\n";
+
+	Pierwsza jedyneczka;
+	Druga dwujeczka;
+
+	Pierwsza *wsk;
+
+	wsk = &jedyneczka;
+	wsk->nazwa();
+
+	wsk = &dwujeczka; //wsk jest typu klasa Pierwsza, bêdzie pokazywa³ na obiekt klasy Druga.
+	wsk->nazwa(); //poniewa¿ funkcja nazwa jest wirtualna to kompilator wykryje ¿e ma u¿ywaæ funkcji klasy Druga.
+
+	dwujeczka.liczba(4);
+	dwujeczka.liczba_v(5);
+	dwujeczka.Pierwsza::liczba(12);
+	dwujeczka.Pierwsza::liczba_v(9);
 
 	wait();
 
@@ -27,7 +43,7 @@ void wait()
 	} while (cin.get() != '\n');
 }
 
-
+// Funkcje o tych samych nazwach prze³adowuj¹ siê.
 int sum(int a, int b)
 {
 	return a + b;
@@ -36,4 +52,42 @@ int sum(int a, int b)
 float sum(float a, float b)
 {
 	return a + b;
+}
+
+void Pierwsza :: liczba(int a)
+{
+	cout << "\nPierwsza liczba ";
+	cout << a;
+	cout << "\n";
+}
+
+void Pierwsza::liczba_v(int a)
+{
+	cout << "\nPierwsza liczba_v ";
+	cout << a;
+	cout << "\n";
+}
+
+void Pierwsza::nazwa()
+{
+	cout << "\nPierwsza\n";
+}
+
+void Druga::nazwa()
+{
+	cout << "\nDruga\n";
+}
+
+void Druga::liczba(int a)
+{
+	cout << "\nDruga liczba ";
+	cout << a;
+	cout << "\n";
+}
+
+void Druga::liczba_v(int a)
+{
+	cout << "\nDruga liczba_v ";
+	cout << a;
+	cout << "\n";
 }
--- polimorfizm/polimorfizm.h
+++ polimorfizm/polimorfizm.h
@@ -3,9 +3,27 @@
 #ifndef _PoliMor
 #define _PoliMor
 
+class Pierwsza
+{
+public:
+	virtual void nazwa();
 
+	virtual void liczba_v(int a);
+	void liczba(int a);
+};
+
+class Druga : public Pierwsza
+{
+public:
+	void nazwa();
+	void liczba_v(int a) override; //funkcja zacieraj¹ca funkcje liczba_v z klasy Pierwsza //s³owo override jest dla kompilatora w celu weryfikacji (nie jest wymagane)
+	void liczba(int a); //funkcja zas³aniaj¹ca funkcje liczba z klasy Pierwsza
+};
+
+// Funkcje o tych samych nazwach prze³adowuj¹ siê.
 int sum(int, int);
 float sum(float, float);
+
 void wait();
 
 #endif
